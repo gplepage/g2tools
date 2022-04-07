21365f8027371f30cbfbeaa1b617e5d412ab268b
G. Peter Lepage <g.p.lepage@cornell.edu>
2022-04-07 14:30:17 -0400

    documentation

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'g2tools'
-copyright = u'2017, G.P. Lepage'
+copyright = u'2017-22, G.P. Lepage'
 author = u'G.P. Lepage'
 
 # The version info for the project you're documenting, acts as replacement for
